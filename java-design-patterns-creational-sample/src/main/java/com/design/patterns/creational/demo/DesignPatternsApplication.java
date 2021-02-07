d3702afb2f59b5e2b72f9f90b641cc29b4096211
lsefiane <lyes.sefiane@gmail.com>
2021-02-07 16:05:14 -0500

    Prototype Design Pattern Sample

--- java-design-patterns-creational-sample/src/main/java/com/design/patterns/creational/demo/DesignPatternsApplication.java
+++ java-design-patterns-creational-sample/src/main/java/com/design/patterns/creational/demo/DesignPatternsApplication.java
@@ -10,6 +10,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.design.patterns.creational.builder.Bedroom;
 import com.design.patterns.creational.builder.BedroomBuilder;
+import com.design.patterns.creational.prototype.Breed;
+import com.design.patterns.creational.prototype.Person;
+import com.design.patterns.creational.prototype.Rabbit;
 import com.design.patterns.creational.singleton.PrintSpooler;
 
 @SpringBootApplication
@@ -21,6 +24,7 @@ public class DesignPatternsApplication {
 
 		SpringApplication.run(DesignPatternsApplication.class, args);
 
+		// Builder Design Pattern Sample.
 		Bedroom bedroom = new BedroomBuilder()//
 				.setDimensions(new Dimension(200, 100))//
 				.setCellingHeight(132).setFloorNumber(2)//
@@ -28,13 +32,22 @@ public class DesignPatternsApplication {
 				.setNumberOfWindows(16)//
 				.setWallColor(Color.CYAN)//
 				.build();
-
 		logger.info("Builder Design Pattern {}", bedroom);
 		
 		
+		// Singleton Design Pattern Sample.
 		PrintSpooler spooler = PrintSpooler.getInstance();
-		
 		logger.info("Singleton Design Pattern {}", spooler);
+		
+		
+		// Prototype Design Pattern Sample.
+		// Original Rabbit
+		Rabbit rabbit = new Rabbit(12, Breed.AMERICAN);
+		rabbit.setOwner(new Person("Sally"));
+		// Cloned Rabbit
+		Rabbit clonedRabbit = rabbit.clone();
+		clonedRabbit.setOwner(new Person("Steve"));
+		logger.info("Prototype Design Pattern {}", clonedRabbit);
 
 	}
 
