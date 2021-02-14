6717f5dd2c4e910d9b525abcc74abb6ca1461b93
lsefiane <lyes.sefiane@gmail.com>
2021-02-14 14:31:37 -0500

    ~ Init

---
+++ java-design-patterns-structural-sample/src/main/java/com/design/patterns/structural/demo/DesignPatternsApplication.java
@@ -0,0 +1,28 @@
+package com.design.patterns.structural.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * DesignPatternsApplication.java
+ *
+ * @author Lyes Sefiane
+ * @email lyes.sefiane@gmail.com
+ * @date Feb. 14, 2021
+ *
+ */
+@SpringBootApplication
+public class DesignPatternsApplication {
+
+	private static Logger logger = LoggerFactory.getLogger(DesignPatternsApplication.class);
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DesignPatternsApplication.class, args);
+
+	}
+
+}
