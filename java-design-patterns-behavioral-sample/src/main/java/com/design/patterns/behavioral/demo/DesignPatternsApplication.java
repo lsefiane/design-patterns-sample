6ad36f321322bb227658371b36c279c0a59b957e
lsefiane <lyes.sefiane@gmail.com>
2021-02-24 16:39:49 -0500

    ~ Init

---
+++ java-design-patterns-behavioral-sample/src/main/java/com/design/patterns/behavioral/demo/DesignPatternsApplication.java
@@ -0,0 +1,21 @@
+package com.design.patterns.behavioral.demo;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class DesignPatternsApplication {
+
+	private static Logger logger = LoggerFactory.getLogger(DesignPatternsApplication.class);
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DesignPatternsApplication.class, args);
+
+	}
+
+}
